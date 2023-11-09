ea055718c407604f43225ff2e1bdbaf292b08bc0
Salar kalantari <salarkalantari91@gmail.com>
2023-11-09 09:19:43 +0100

    Alloy grammar added

--- frontend/main.js
+++ frontend/main.js
@@ -157,6 +157,84 @@ const smt2_lang = {
 
 /* ---------------End SMT Grammer--------------- */
 
+/* --------------Start Alloy Grammar------------ */
+const alloy_lang = {
+
+  keywords: [
+    'one', 'lone', 'none', 'some', 'abstract', 'all', 'iff', 'but', 'else', 'extends', 'set', 'implies', 
+    'module', 'open', 'and', 'disj', 'for', 'in', 'no', 'or', 'as', 'Int', 'String', 'sum', 'exactly', 
+    'iden', 'let', 'not', 'univ', 'enum', 'var', 'steps', 'always', 'historically', 'eventually', 'once', 
+    'after', 'before', 'until', 'since', 'releases', 'triggered', 'check', 'fact', 'sig', 'fun', 'pred', 
+    'assert', 'run',
+  ],
+
+  operators: [
+    '=>', '<=>', '++', '=<', '->', '>=', '||', '<:', ':>', '&&', '!=', '+', '-', '&', '.', '~', '*', '^',
+    '!', '#',
+  ],
+
+  // we include these common regular expressions
+  symbols:  /[=><!~?:&|+\-*\/\^%]+/,
+
+  escapes: /\\(?:[abfnrtv\\"']|x[0-9A-Fa-f]{1,4}|u[0-9A-Fa-f]{4}|U[0-9A-Fa-f]{8})/,
+
+  // The main tokenizer for our languages
+  tokenizer: {
+    root: [
+      // identifiers and keywords
+      [/[a-zA-Z_][\w$]*/, { cases: { '@keywords': 'keyword',
+                                   '@default': 'identifier' } }],
+      [/[A-Z][\w\$]*/, 'type.identifier' ], 
+
+      // whitespace
+      { include: '@whitespace' },
+
+      // delimiters and operators
+      [/[{}()\[\]]/, '@brackets'],
+      [/[<>](?!@symbols)/, '@brackets'],
+      [/@symbols/, { cases: { '@operators': 'operator',
+                              '@default'  : '' } } ],
+
+      // numbers
+      [/[0-9]/, 'number'],
+
+      // delimiter: after number because of .\d floats
+      [/[;,.]/, 'delimiter'],
+
+      // strings
+      [/"([^"\\]|\\.)*$/, 'string.invalid' ],  // non-teminated string
+      [/"/,  { token: 'string.quote', bracket: '@open', next: '@string' } ],
+
+      // characters
+      [/'[^\\']'/, 'string'],
+      [/(')(@escapes)(')/, ['string','string.escape','string']],
+      [/'/, 'string.invalid'],
+    ],
+
+    comment: [
+      [/[^\/*]+/, 'comment' ],
+      [/\/\*/,    'comment', '@push' ],    // nested comment
+      ["\\*/",    'comment', '@pop'  ],
+      [/[\/*]/,   'comment' ]
+    ],
+
+    string: [
+      [/[^\\"]+/,  'string'],
+      [/@escapes/, 'string.escape'],
+      [/\\./,      'string.escape.invalid'],
+      [/"/,        { token: 'string.quote', bracket: '@close', next: '@pop' } ],
+    ],
+
+    whitespace: [
+      [/[ \t\r\n]+/, 'white'],
+      [/\/\*/,       'comment', '@comment' ],
+      [/\/\/.*$/,    'comment'],
+      [/\s*--.*$/,    'comment'],
+    ],
+  },
+};
+/* --------------End Alloy Grammar------------ */
+
 /* ---------------Start nuXmv Grammer--------------- */
 const nuXmv_lang = {
 
