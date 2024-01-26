1989ef51d83abc3d0e6f88fd99b32d08a24c1b77
azadP2ER <azad_dima@outlook.com>
2024-01-26 18:30:29 +0100

    feat(editor): add smt language support

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,40 +1,42 @@
-import React, { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import React, {useState, useEffect} from 'react'
+import {BrowserRouter as Router, Route, Routes} from 'react-router-dom'
 import './assets/style/index.css'
 import Nav from './components/Utils/Nav'
 import Footer from './components/Utils/Footer'
 import Playground from './components/Playground/Playground'
 import Missing from './components/Utils/Missing'
 
+// { id: "smt2",  value: "3", label: "SMT", short: "SMT" },
 
 const App = () => {
-  const [editorValue, setEditorValue] = useState('');
-  const [language, setLanguage] = useState('');
-  const handleEditorValueChange = (code) => {
-    setEditorValue(code);
-  };
+    const [editorValue, setEditorValue] = useState('');
+    const [language, setLanguage] = useState('');
 
+    // eslint-disable-next-line no-unused-vars
+    const handleEditorValueChange = (code) => {
+        setEditorValue(code);
+    };
 
-  return (
-      <div>
-        <Nav
-          setEditorValue={setEditorValue}
-          setLanguage={setLanguage}
-        />
-        <Router>
-          <Routes>
-            <Route path="/" element={<Playground
-              editorValue={editorValue}
-              setEditorValue={setEditorValue}
-              language={language}
-              setLanguage={setLanguage}
-            />} />
-            <Route path="*" element={<Missing />} />
-          </Routes>
-        </Router>
-        <Footer />
-      </div>
-  )
+    return (
+        <div>
+            <Nav
+                setEditorValue={setEditorValue}
+                setLanguage={setLanguage}
+            />
+            <Router>
+                <Routes>
+                    <Route path="/" element={
+                        <Playground editorValue={editorValue}
+                                    setEditorValue={setEditorValue}
+                                    language={{id: "smt2", value: "3", label: "SMT", short: "SMT"}}
+                                    setLanguage={setLanguage}/>}
+                    />
+                    <Route path="*" element={<Missing/>}/>
+                </Routes>
+            </Router>
+            <Footer/>
+        </div>
+    )
 }
 
 export default App
--- frontend/src/components/Playground/Editor.jsx
+++ frontend/src/components/Playground/Editor.jsx
@@ -1,126 +1,124 @@
-import { useState, useRef, useEffect } from 'react'
+import {useState, useRef, useEffect} from 'react'
 import Editor from "@monaco-editor/react";
-import { limbooleConf, limbooleLang } from '../../assets/languages/limboole'
-import { smt2Conf, smt2Lang, smt2ComplitionProvider } from '../../assets/languages/smt2'
-import { nuxmvConf, nuxmvLang } from '../../assets/languages/nuxmv'
-import { alloyConf, alloyLang } from '../../assets/languages/alloy'
+import {limbooleConf, limbooleLang} from '../../assets/languages/limboole'
+import {smt2Conf, smt2Lang, smt2ComplitionProvider} from '../../assets/languages/smt2'
+import {nuxmvConf, nuxmvLang} from '../../assets/languages/nuxmv'
+import {alloyConf, alloyLang} from '../../assets/languages/alloy'
 
 
 /**
  * Code editor component.
- * @param {*} props 
- * @returns 
+ * @param {*} props
+ * @returns
  */
 const CodeEditor = (props) => {
-  const editorRef = useRef(null) // editor reference
-  const [internalEditorValue, setInternalEditorValue] = useState(props.editorValue);
-  const [language, setLanguage] = useState(props.language.id);
+    const editorRef = useRef(null) // editor reference
+    const [internalEditorValue, setInternalEditorValue] = useState(props.editorValue);
+    const [language, setLanguage] = useState(props.language.id);
 
-  /**
-  * Sets the editor value when the editorValue prop changes.
-  */
-  useEffect(() => {
-    setInternalEditorValue(props.editorValue);
-  }, [props.editorValue]);
+    /**
+     * Sets the editor value when the editorValue prop changes.
+     */
+    useEffect(() => {
+        setInternalEditorValue(props.editorValue);
+    }, [props.editorValue]);
 
-  /**
-   * Sets the language when the language prop changes.
-  */
-  useEffect(() => {
-    setLanguage(props.language.id);
-  }, [props.language.id]);
+    /**
+     * Sets the language when the language prop changes.
+     */
+    useEffect(() => {
+        setLanguage(props.language.id);
+    }, [props.language.id]);
 
+    /**
+     * Handles the editor did mount event. On mount, registers all the languages.
+     * Languages and their configurations are defined in the assets/languages folder.
+     * @see assets/languages
+     * @param {*} editor // editor reference
+     * @param {*} monaco
+     * @todo Add autocomplete for the languages (monaco.languages.registerCompletionItemProvider)
+     */
+    function handleEditorDidMount(editor, monaco) {
+        editorRef.current = editor
+        editorRef.current.focus()
+        // register limboole language
+        monaco.languages.register({id: 'limboole'})
+        monaco.languages.setMonarchTokensProvider('limboole', limbooleLang)
+        monaco.languages.setLanguageConfiguration('limboole', limbooleConf)
 
-  /**
-   * Handles the editor did mount event. On mount, registers all the languages. 
-   * Languages and their configurations are defined in the assets/languages folder.
-   * @see assets/languages
-   * @param {*} editor // editor reference
-   * @param {*} monaco  
-   * @todo Add autocomplete for the languages (monaco.languages.registerCompletionItemProvider)
-   */
-  function handleEditorDidMount(editor, monaco) {
-    editorRef.current = editor
-    editorRef.current.focus()
-    // register limboole language
-    monaco.languages.register({ id: 'limboole' })
-    monaco.languages.setMonarchTokensProvider('limboole', limbooleLang)
-    monaco.languages.setLanguageConfiguration('limboole', limbooleConf)
+        // register smt2 language
+        monaco.languages.register({id: 'smt2'})
+        monaco.languages.setMonarchTokensProvider('smt2', smt2Lang)
+        monaco.languages.setLanguageConfiguration('smt2', smt2Conf)
+        monaco.languages.registerCompletionItemProvider('smt2', smt2ComplitionProvider);
 
-    // register smt2 language
-    monaco.languages.register({ id: 'smt2' })
-    monaco.languages.setMonarchTokensProvider('smt2', smt2Lang)
-    monaco.languages.setLanguageConfiguration('smt2', smt2Conf)
-    monaco.languages.registerCompletionItemProvider('smt2', smt2ComplitionProvider);
+        // register nuxmv language
+        monaco.languages.register({id: 'xmv'})
+        monaco.languages.setMonarchTokensProvider('xmv', nuxmvLang)
+        monaco.languages.setLanguageConfiguration('xmv', nuxmvConf)
 
-    // register nuxmv language
-    monaco.languages.register({ id: 'xmv' })
-    monaco.languages.setMonarchTokensProvider('xmv', nuxmvLang)
-    monaco.languages.setLanguageConfiguration('xmv', nuxmvConf)
+        // register alloy language
+        monaco.languages.register({id: 'als'})
+        monaco.languages.setMonarchTokensProvider('als', alloyLang)
+        monaco.languages.setLanguageConfiguration('als', alloyConf)
+    }
 
-    // register alloy language
-    monaco.languages.register({ id: 'als' })
-    monaco.languages.setMonarchTokensProvider('als', alloyLang)
-    monaco.languages.setLanguageConfiguration('als', alloyConf)
-  }
+    /**
+     * Gets the editor value and sets the editorValue prop.
+     * @param {*} value
+     * @param {*} event
+     */
+    function getEditorValue(value, event) {
+        editorRef.current.getValue()
+        props.setEditorValue(editorRef.current.getValue())
+    }
 
-  /**
-   * Gets the editor value and sets the editorValue prop.
-   * @param {*} value 
-   * @param {*} event 
-   */
-  function getEditorValue(value, event) {
-    editorRef.current.getValue()
-    props.setEditorValue(editorRef.current.getValue())
-  }
-
-  /**
-   * Sets the editor value.
-   * @param {*} value
-   * @param {*} event
-   */
-  function setEditorValue(value, event) {
-    editorRef.current.setValue(value)
-  }
-
+    /**
+     * Sets the editor value.
+     * @param {*} value
+     * @param {*} event
+     */
+    function setEditorValue(value, event) {
+        editorRef.current.setValue(value)
+    }
 
 
-  /**
-   * Handles the code change event.
-   * Sets the editor value with the new code.
-   * @param {*} newCode 
-   */
-  const handleCodeChange = (newCode) => {
-    props.setEditorValue(newCode)
-  }
+    /**
+     * Handles the code change event.
+     * Sets the editor value with the new code.
+     * @param {*} newCode
+     */
+    const handleCodeChange = (newCode) => {
+        props.setEditorValue(newCode)
+    }
 
-  return (
-    <>
-      <div className="App">
-        <Editor
-          height={props.height}
-          width="100%"
-          language={language}
-          defaultValue="Write your code here"
-          value={internalEditorValue}
-          theme={props.theme}
-          options={{
-            minimap: {
-              enabled: false,
-            },
-            automaticLayout: true,
-            mouseWheelZoom: true,
-            bracketPairColorizationOptions: {
-              enabled: true,
-              independentColorPoolPerBracketType: true,
-            },
-          }}
-          onMount={handleEditorDidMount}
-          onChange={handleCodeChange}
-        />
-      </div>
-    </>
-  )
+    return (
+        <>
+            <div className="App">
+                <Editor
+                    height={props.height}
+                    width="100%"
+                    language={language}
+                    defaultValue=";Write your code here"
+                    value={internalEditorValue}
+                    theme={props.theme}
+                    options={{
+                        minimap: {
+                            enabled: false,
+                        },
+                        automaticLayout: true,
+                        mouseWheelZoom: true,
+                        bracketPairColorizationOptions: {
+                            enabled: true,
+                            independentColorPoolPerBracketType: true,
+                        },
+                    }}
+                    onMount={handleEditorDidMount}
+                    onChange={handleCodeChange}
+                />
+            </div>
+        </>
+    )
 }
 
 export default CodeEditor;
