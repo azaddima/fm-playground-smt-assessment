ef1c91b5d3d378b98aad410b3f5e2ec61b931da9
Husain Saif <husainsaif@Husains-MacBook-Pro.local>
2024-01-29 08:39:23 +0100

    checking equivalence1

--- backend/smt_assessment/boolean_task1.py
+++ backend/smt_assessment/boolean_task1.py
@@ -3,14 +3,16 @@ sys.path.append("..")  # Adds higher directory to python modules path.
 from z3 import *
 
 p, q, r = Bools('p, q, r')
-S = Solver
+S = Solver()
 f = parse_smt2_file("smtlib_examples//boolean_intro.smt2")
-S.check(f)
-
+f_bool = BoolVector( f,  len(f))
+print(f_bool)
 p, q, r = Bools('p, q, r')
 check_f = Implies(p, Or(And(q, Not(r), And(Not(q), r))))
-print(type(check_f))
-new_from_front = f == check_f
+
+S.add(f)
+new_from_front = Bool(obj_to_string(f)) == check_f
+
 
 
 
@@ -19,7 +21,7 @@ S.add(Not(new_from_front))
 if S.check() == unsat:
     print("correct evaluation of the task given")
 else:
-    print("please try again at line 6")
+    print( "try again at line 6")
 
 
 #[Implies(p, Or(And(q, Not(r), And(Not(q), r))))]
