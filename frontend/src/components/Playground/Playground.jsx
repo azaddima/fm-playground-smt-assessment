d5f53e063cc9a822149568e7aba14a2a1a645deb
azadP2ER <azad_dima@outlook.com>
2024-03-17 20:33:27 +0100

    decision tree images

--- frontend/src/components/Playground/Playground.jsx
+++ frontend/src/components/Playground/Playground.jsx
@@ -15,7 +15,7 @@ const Playground = ({editorValue, setEditorValue, language, setLanguage}) => {
         setOutput('')
         try {
             setIsExecuting(true);
-            
+
 
             // Appends currentTask value to editorValue for loading the correct assessment
             executeCmdTool(currentTask + editorValue)
@@ -48,15 +48,16 @@ const Playground = ({editorValue, setEditorValue, language, setLanguage}) => {
             }
         };
 
+        if (currentTask === "9" || currentTask === "10") {
+            console.log("hi")
+        } else {
+            const text = loadFile(currentTask);
+            text.then(result => {
+                setEditorValue(result);
+            })
+        }
         // Call the loadFile function
-        const text = loadFile(currentTask);
-        text.then(result => {
-            setEditorValue(result);
-        })
-
-
     }, [currentTask]);
-
 
     /**
      * Update the output area with the code passed as a prop to the PlainOutput component.
@@ -67,64 +68,77 @@ const Playground = ({editorValue, setEditorValue, language, setLanguage}) => {
     };
 
     return (
-        <div className="container mt-5 pt-5">
-            <select value={currentTask} onChange={e => setCurrentTask(e.target.value)}>
-                <option value={"1"}>Lecture 1</option>
-                <option value={"3"}>Boolean Task</option>
-                <option value={"4"}>Mathematics</option>
-                <option value={"5"}>Check Theory</option>
-                <option value={"6"}>MCQS</option>
-                <option value={"7"}>Knapsack</option>
-                <option value={"8"}>Decision Trees</option>
-            </select>
-            <div className="row">
-                <div className="col-md-6" style={{backgroundColor: 'white'}}>
-                    <div className='row'>
-                        <div className='col-md-12 mx-auto mb-2'>
-                            <div className='d-flex justify-content-between'>
-                                <div className='col-md-4'>
-                                    <h2>Input</h2>
-                                </div>
+        <>
+            <div className="container mt-5 pt-5">
+                <div className="row">
+                    <select value={currentTask}
+                            onChange={e => setCurrentTask(e.target.value)}>
+                        <option value={"1"}>Lecture 1</option>
+                        <option value={"3"}>Boolean Task</option>
+                        <option value={"4"}>Mathematics</option>
+                        <option value={"5"}>Check Theory</option>
+                        <option value={"6"}>MCQS</option>
+                        <option value={"7"}>Knapsack</option>
+                        <option value={"8"}>Decision Trees</option>
+                    </select>
+                    {currentTask === "8" &&
+                        <div className="row mt-5 mb-5 ">
+                            <h4>Original Tree</h4>
+                            <img className="" src="original_decision_tree.png"/>
+                            <h4 className="mt-5">Pruned Tree</h4>
+                            <img className="" src="pruned_decision_tree.png"/>
+                        </div>
+                    }
 
+                    <div className="col-md-6" style={{backgroundColor: 'white'}}>
+                        <div className='row'>
+                            <div className='col-md-12 mx-auto mb-2'>
+                                <div className='d-flex justify-content-between'>
+                                    <div className='col-md-4'>
+                                        <h2>Input</h2>
+                                    </div>
+
+                                </div>
                             </div>
-                        </div>
-                        <Editor
-                            height='60vh'
-                            setEditorValue={setEditorValue}
-                            editorValue={editorValue}
-                            language={language}
-                            setLanguage={setLanguage}
-                            theme='vs-dark'
-                            currentTask={currentTask}
-                        />
-                        <MDBBtn
-                            className='mx-auto my-3'
-                            style={{width: '95%'}}
-                            color='primary'
-                            onClick={handleToolExecution}
-                            disabled={isExecuting}
-                        >
-                            {isExecuting ? 'Running...' : 'RUN'}
-                        </MDBBtn>
-                    </div>
-                </div>
-                <div className='col-md-6' style={{backgroundColor: 'white'}}>
-                    <div className='row'>
-                        <div className='col-md-12'>
-                            <div className={`d-flex justify-content-between ${language.id !== 'xmv' ? 'mb-3' : ''}`}>
-                                <h2>Output</h2>
-                            </div>
-                        </div>
-                        <div className='col-md-12'>
-                            <PlainOutput
-                                code={output}
+                            <Editor
                                 height='60vh'
-                                onChange={handleOutputChange}/>
+                                setEditorValue={setEditorValue}
+                                editorValue={editorValue}
+                                language={language}
+                                setLanguage={setLanguage}
+                                theme='vs-dark'
+                                currentTask={currentTask}
+                            />
+                            <MDBBtn
+                                className='mx-auto my-3'
+                                style={{width: '95%'}}
+                                color='primary'
+                                onClick={handleToolExecution}
+                                disabled={isExecuting}
+                            >
+                                {isExecuting ? 'Running...' : 'RUN'}
+                            </MDBBtn>
+                        </div>
+                    </div>
+                    <div className='col-md-6' style={{backgroundColor: 'white'}}>
+                        <div className='row'>
+                            <div className='col-md-12'>
+                                <div
+                                    className={`d-flex justify-content-between ${language.id !== 'xmv' ? 'mb-3' : ''}`}>
+                                    <h2>Output</h2>
+                                </div>
+                            </div>
+                            <div className='col-md-12'>
+                                <PlainOutput
+                                    code={output}
+                                    height='60vh'
+                                    onChange={handleOutputChange}/>
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
-        </div>
+        </>
     )
 }
 
